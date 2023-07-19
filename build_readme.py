862e690d767a33d85b61510347c6d61bf44a3dd5
fulln <245193247@qq.com>
2023-07-20 00:32:28 +0800

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -69,7 +69,7 @@ def fetch_releases(oauth_token):
             headers={"Authorization": "Bearer {}".format(oauth_token)},
         )
         print()
-        print(json.dumps(data, indent=4))
+        print(json.dumps(data, ensure_ascii=False))
         print()
         for repo in data["data"]["viewer"]["repositories"]["nodes"]:
             if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
